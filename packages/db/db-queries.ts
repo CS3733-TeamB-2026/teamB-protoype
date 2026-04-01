432a63155e553aa2882c1b0eec31f212706418bf
LukeCiar <lukemciarletta@gmail.com>
2026-04-01 19:56:28 -0400

    Added queryAllEmployees()

--- packages/db/db-queries.ts
+++ packages/db/db-queries.ts
@@ -1,17 +1,11 @@
 import { prisma } from "./lib/prisma"
 
 async function queryAllEmployees() {
-    const employees = await prisma.employee.findMany({});
-    return employees;
+    const employees = await prisma.employee.findMany({})
+    //return JSON.stringify(employees, null, 2)
+    return employees
 }
 
-const employees = queryAllEmployees()
-    .then(async () => {
-    await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
-console.log("All employees:", JSON.stringify(employees, null, 2));
+queryAllEmployees().then(
+    (employees) => {console.log(employees)}
+)
