f2d6bfd07c147cd6142304105888543bc050af98
LukeCiar <lukemciarletta@gmail.com>
2026-04-01 20:43:34 -0400

    Fixed queryEmployees and added createEmployee

--- packages/db/db-queries.ts
+++ packages/db/db-queries.ts
@@ -1,4 +1,5 @@
 import { prisma } from "./lib/prisma"
+import { Persona } from "./generated/prisma/client"
 
 async function queryAllEmployees() {
     const employees = await prisma.employee.findMany({})
@@ -6,6 +7,27 @@ async function queryAllEmployees() {
     return employees
 }
 
-queryAllEmployees().then(
-    (employees) => {console.log(employees)}
-)
+async function createEmployee(_id: number, _firstName: string, _lastName: string, _persona: string | null) {
+    const personaTyped = employeePersonaHelper(_persona)
+    const employee = await prisma.employee.create({
+        data: {
+            id: _id,
+            firstName: _firstName,
+            lastName: _lastName,
+            persona: personaTyped
+        }
+    })
+}
+
+function employeePersonaHelper(_persona: string | null) {
+    if (_persona == "underwriter") { return Persona.underwriter }
+    else if (_persona == "businessAnalyst") { return Persona.businessAnalyst }
+    else { return null }
+}
+
+//queryAllEmployees().then(
+    //(employees) => {console.log(employees)})
+
+//createEmployee(27, "John", "NoRole").then(
+//    () => queryAllEmployees().then(
+//        (employees) => {console.log(employees)}))
