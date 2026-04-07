c5dcb0ea18b39c9c4852bf7ce1da4f1bc2eb26a1
Oscar Stomberg <oscar@stomberg.us>
2026-04-07 18:16:04 -0400

    Undid auto-formatting

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -13,44 +13,44 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/api/employee", async (req, res) => {
-  try {
-    const employees = await q.queryAllEmployees();
-    res.status(200).json(employees);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    try {
+        const employees = await q.queryAllEmployees();
+        res.status(200).json(employees);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 app.get("/api/content", async (req, res) => {
-  try {
-    const employees = await q.queryAllContent();
-    res.status(200).json(employees);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    try {
+        const employees = await q.queryAllContent();
+        res.status(200).json(employees);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 app.get("/api/servicereqs", async (req, res) => {
-  try {
-    const servicereqs = await q.queryAllServiceReqs();
-    res.status(200).json(servicereqs);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    try {
+        const servicereqs = await q.queryAllServiceReqs();
+        res.status(200).json(servicereqs);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 app.get("/api/assigned", async (req, res) => {
-  try {
-    const id = parseInt(req.query.id as string);
-    const assigned = await q.queryAssignedServiceReqs();
-    res.status(200).json(assigned);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    try {
+        const id = parseInt(req.query.id as string);
+        const assigned = await q.queryAssignedServiceReqs();
+        res.status(200).json(assigned);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 /*
@@ -66,130 +66,130 @@ app.get("/api/files", async (req, res) => {
 */
 
 app.post("/api/employee", async (req, res) => {
-  const payload = req.body;
-  try {
-    const result = await q.createEmployee(
-      payload.firstName,
-      payload.lastName,
-      payload.id,
-      payload.persona,
-    );
-    res.status(201).json(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    const payload = req.body;
+    try {
+        const result = await q.createEmployee(
+            payload.firstName,
+            payload.lastName,
+            payload.id,
+            payload.persona,
+        );
+        res.status(201).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 app.post("/api/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const login = await q.queryLoginByUsername(username);
+    try {
+        const { username, password } = req.body;
+        const login = await q.queryLoginByUsername(username);
 
-    if (!login) {
-      res.status(401).json({ message: "User not found" });
+        if (!login) {
+            res.status(401).json({ message: "User not found" });
+        }
+
+        if (login.password !== password) {
+            res.status(401).json({ message: "Incorrect Password" });
+        }
+
+        const employee = await q.queryEmployeeById(login.id);
+        res.status(200).json(employee);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
     }
-
-    if (login.password !== password) {
-      res.status(401).json({ message: "Incorrect Password" });
-    }
-
-    const employee = await q.queryEmployeeById(login.id);
-    res.status(200).json(employee);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
 });
 
 app.post("/api/content", upload.single("file"), async (req, res) => {
-  const payload = req.body;
-  try {
-    let fileURI = null;
-    if (req.file) {
-      fileURI =
-        (payload.ownerID as String) +
-        "/" +
-        crypto.randomUUID() +
-        "/" +
-        req.file.originalname;
-      const uploadResult = await q.uploadFile(req.file.buffer, fileURI);
-      fileURI = uploadResult.path;
+    const payload = req.body;
+    try {
+        let fileURI = null;
+        if (req.file) {
+            fileURI =
+                (payload.ownerID as String) +
+                "/" +
+                crypto.randomUUID() +
+                "/" +
+                req.file.originalname;
+            const uploadResult = await q.uploadFile(req.file.buffer, fileURI);
+            fileURI = uploadResult.path;
+        }
+        const result = await q.createContent(
+            payload.name,
+            payload.linkURL || null,
+            fileURI,
+            payload.ownerID ? parseInt(payload.ownerID) : null,
+            payload.contentType,
+            payload.status,
+            new Date(payload.lastModified),
+            payload.expiration ? new Date(payload.expiration) : null,
+            payload.jobPosition,
+        );
+        res.status(201).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
     }
-    const result = await q.createContent(
-      payload.name,
-      payload.linkURL || null,
-      fileURI,
-      payload.ownerID ? parseInt(payload.ownerID) : null,
-      payload.contentType,
-      payload.status,
-      new Date(payload.lastModified),
-      payload.expiration ? new Date(payload.expiration) : null,
-      payload.jobPosition,
-    );
-    res.status(201).json(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
 });
 
 app.put("/api/employee", async (req, res) => {
-  const payload = req.body;
-  try {
-    const result = await q.updateEmployee(
-      payload.id,
-      payload.firstName,
-      payload.lastName,
-      payload.persona,
-    );
-    res.status(200).json(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    const payload = req.body;
+    try {
+        const result = await q.updateEmployee(
+            payload.id,
+            payload.firstName,
+            payload.lastName,
+            payload.persona,
+        );
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 app.put("/api/content", async (req, res) => {
-  const payload = req.body;
-  try {
-    const result = await q.updateContent(
-      payload.name,
-      payload.linkURL,
-      payload.ownerID,
-      payload.contentType,
-      payload.status,
-      payload.lastModified,
-      payload.expiration,
-      payload.jobPosition,
-    );
-    res.status(200).json(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    const payload = req.body;
+    try {
+        const result = await q.updateContent(
+            payload.name,
+            payload.linkURL,
+            payload.ownerID,
+            payload.contentType,
+            payload.status,
+            payload.lastModified,
+            payload.expiration,
+            payload.jobPosition,
+        );
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 app.delete("/api/employee", async (req, res) => {
-  const payload = req.body;
-  try {
-    const result = await q.deleteEmployee(payload.id);
-    res.status(204).json(result); // 204 since no object remains
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    const payload = req.body;
+    try {
+        const result = await q.deleteEmployee(payload.id);
+        res.status(204).json(result); // 204 since no object remains
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 app.delete("/api/content", async (req, res) => {
-  const payload = req.body;
-  try {
-    const result = await q.deleteContent(payload.name);
-    res.status(204).json(result); // 204 since no object remains
-  } catch (error) {
-    console.error(error);
-    res.status(500).end();
-  }
+    const payload = req.body;
+    try {
+        const result = await q.deleteContent(payload.name);
+        res.status(204).json(result); // 204 since no object remains
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
+    }
 });
 
 /*
@@ -199,6 +199,6 @@ app.delete("/api/content", async (req, res) => {
     */
 
 app.listen(3000, () => {
-  console.log(`Server is listening on port 3000`);
-  console.log(`    http://localhost:3000`);
+    console.log(`Server is listening on port 3000`);
+    console.log(`    http://localhost:3000`);
 });
--- packages/db/db-queries.ts
+++ packages/db/db-queries.ts
@@ -6,153 +6,153 @@ const bucket = "content";
 
 // Employee Queries
 export async function queryAllEmployees(): Promise<p.Employee[]> {
-  return prisma.employee.findMany({});
+    return prisma.employee.findMany({});
 }
 
 export async function queryEmployeeById(
-  id: number,
+    id: number,
 ): Promise<p.Employee | null> {
-  return prisma.employee.findUnique({
-    where: { id: id },
-  });
+    return prisma.employee.findUnique({
+        where: { id: id },
+    });
 }
 
 export async function updateEmployee(
-  id: number,
-  _firstName: string,
-  _lastName: string,
-  _persona: string | null,
+    id: number,
+    _firstName: string,
+    _lastName: string,
+    _persona: string | null,
 ): Promise<void> {
-  const personaTyped: p.Persona | null = personaHelper(_persona);
-  const updatedUser = await prisma.employee.update({
-    where: { id: id },
-    data: {
-      firstName: _firstName,
-      lastName: _lastName,
-      persona: personaTyped,
-    },
-  });
+    const personaTyped: p.Persona | null = personaHelper(_persona);
+    const updatedUser = await prisma.employee.update({
+        where: { id: id },
+        data: {
+            firstName: _firstName,
+            lastName: _lastName,
+            persona: personaTyped,
+        },
+    });
 }
 
 export async function uploadFile(file: Buffer, path: string) {
-  const { data, error } = await supabase.storage
-    .from(bucket)
-    .upload(path, file);
-  if (error) throw error;
-  return data;
+    const { data, error } = await supabase.storage
+        .from(bucket)
+        .upload(path, file);
+    if (error) throw error;
+    return data;
 }
 
 export async function downloadFile(path: string) {
-  const { data, error } = await supabase.storage.from(bucket).download(path);
-  if (error) throw error;
-  return data;
+    const { data, error } = await supabase.storage.from(bucket).download(path);
+    if (error) throw error;
+    return data;
 }
 
 export async function createContent(
-  _name: string,
-  _linkURL: string | null,
-  _fileURI: string | null,
-  _ownerID: number | null,
-  _contentType: p.ContentType,
-  _status: p.Status | null,
-  _lastModified: Date,
-  _expiration: Date | null,
-  _targetPersona: string,
+    _name: string,
+    _linkURL: string | null,
+    _fileURI: string | null,
+    _ownerID: number | null,
+    _contentType: p.ContentType,
+    _status: p.Status | null,
+    _lastModified: Date,
+    _expiration: Date | null,
+    _targetPersona: string,
 ): Promise<p.Content> {
-  if (!_linkURL && !_fileURI) {
-    throw new Error("Content must have either a linkURL or a fileURI");
-  }
-  if (_linkURL && _fileURI) {
-    throw new Error("Content cannot have both a linkURL and a fileURI");
-  }
-  const _personaTyped: p.Persona = personaHelper(_targetPersona);
-  const _statusTyped: p.Status = statusHelper(_status);
-  return prisma.content.create({
-    data: {
-      name: _name,
-      linkURL: _linkURL,
-      fileURI: _fileURI,
-      ownerID: _ownerID,
-      contentType: _contentType,
-      status: _statusTyped,
-      lastModified: _lastModified,
-      expiration: _expiration,
-      targetPersona: _personaTyped,
-    },
-  });
+    if (!_linkURL && !_fileURI) {
+        throw new Error("Content must have either a linkURL or a fileURI");
+    }
+    if (_linkURL && _fileURI) {
+        throw new Error("Content cannot have both a linkURL and a fileURI");
+    }
+    const _personaTyped: p.Persona = personaHelper(_targetPersona);
+    const _statusTyped: p.Status = statusHelper(_status);
+    return prisma.content.create({
+        data: {
+            name: _name,
+            linkURL: _linkURL,
+            fileURI: _fileURI,
+            ownerID: _ownerID,
+            contentType: _contentType,
+            status: _statusTyped,
+            lastModified: _lastModified,
+            expiration: _expiration,
+            targetPersona: _personaTyped,
+        },
+    });
 }
 
 export async function updateContent(
-  _name: string,
-  _linkURL: string | null,
-  _ownerID: number | null,
-  _contentType: p.ContentType,
-  _status: p.Status | null,
-  _lastModified: Date,
-  _expiration: Date | null,
-  _targetPersona: string,
+    _name: string,
+    _linkURL: string | null,
+    _ownerID: number | null,
+    _contentType: p.ContentType,
+    _status: p.Status | null,
+    _lastModified: Date,
+    _expiration: Date | null,
+    _targetPersona: string,
 ): Promise<void> {
-  const _personaTyped: p.Persona = personaHelper(_targetPersona);
-  const _statusTyped: p.Status = statusHelper(_status);
-  const updatedContent = await prisma.content.update({
-    where: { name: _name },
-    data: {
-      name: _name,
-      linkURL: _linkURL,
-      ownerID: _ownerID,
-      contentType: _contentType,
-      status: _statusTyped,
-      lastModified: _lastModified,
-      expiration: _expiration,
-      targetPersona: _personaTyped,
-    },
-  });
+    const _personaTyped: p.Persona = personaHelper(_targetPersona);
+    const _statusTyped: p.Status = statusHelper(_status);
+    const updatedContent = await prisma.content.update({
+        where: { name: _name },
+        data: {
+            name: _name,
+            linkURL: _linkURL,
+            ownerID: _ownerID,
+            contentType: _contentType,
+            status: _statusTyped,
+            lastModified: _lastModified,
+            expiration: _expiration,
+            targetPersona: _personaTyped,
+        },
+    });
 }
 
 export async function deleteEmployee(id: number): Promise<void> {
-  const deletedUser = await prisma.employee.delete({
-    where: { id: id },
-  });
+    const deletedUser = await prisma.employee.delete({
+        where: { id: id },
+    });
 }
 
 export async function deleteContent(name: string): Promise<void> {
-  const deletedContent = await prisma.content.delete({
-    where: { name: name },
-  });
+    const deletedContent = await prisma.content.delete({
+        where: { name: name },
+    });
 }
 
 // Content Queries
 export async function queryAllContent(): Promise<p.Content[]> {
-  return prisma.content.findMany({});
+    return prisma.content.findMany({});
 }
 
 export async function queryContentByName(
-  name: string,
+    name: string,
 ): Promise<p.Content | null> {
-  return prisma.content.findFirst({
-    where: { name: name },
-    //TODO: Maybe add case insensitivity
-    //TODO: Change to return a list
-    //  Perhaps better to grab ALL filenames so a fuzzy search may be done - Oscar
-  });
+    return prisma.content.findFirst({
+        where: { name: name },
+        //TODO: Maybe add case insensitivity
+        //TODO: Change to return a list
+        //  Perhaps better to grab ALL filenames so a fuzzy search may be done - Oscar
+    });
 }
 
 export async function queryAllServiceReqs(): Promise<p.ServiceRequest[]> {
-  return prisma.serviceRequest.findMany({});
+    return prisma.serviceRequest.findMany({});
 }
 
 export async function queryAssignedServiceReqs(): Promise<p.ServiceRequest[]> {
-  return prisma.serviceRequest.findMany({
-    where: { assigneeID: { not: null } },
-  });
+    return prisma.serviceRequest.findMany({
+        where: { assigneeID: { not: null } },
+    });
 }
 
 export async function queryServiceReqsByAssigned(
-  id: number,
+    id: number,
 ): Promise<p.ServiceRequest[]> {
-  return prisma.serviceRequest.findMany({
-    where: { assigneeID: id },
-  });
+    return prisma.serviceRequest.findMany({
+        where: { assigneeID: id },
+    });
 }
 
 /*
@@ -167,52 +167,52 @@ export async function queryObjectsByBucket(name: string): Promise<p.objects[]> {
  */
 
 export async function createEmployee(
-  _id: number,
-  _firstName: string,
-  _lastName: string,
-  _persona: string | null,
+    _id: number,
+    _firstName: string,
+    _lastName: string,
+    _persona: string | null,
 ): Promise<void> {
-  const _personaTyped: p.Persona = personaHelper(_persona);
-  await prisma.employee.create({
-    data: {
-      id: _id,
-      firstName: _firstName,
-      lastName: _lastName,
-      persona: _personaTyped,
-    },
-  });
+    const _personaTyped: p.Persona = personaHelper(_persona);
+    await prisma.employee.create({
+        data: {
+            id: _id,
+            firstName: _firstName,
+            lastName: _lastName,
+            persona: _personaTyped,
+        },
+    });
 }
 
 export async function queryLoginByUsername(username: string) {
-  return {
-    id: 1,
-    username: "admin",
-    password: "admin",
-  };
+    return {
+        id: 1,
+        username: "admin",
+        password: "admin",
+    };
 }
 
 function personaHelper(_persona: string | null): p.Persona {
-  if (_persona === "underwriter") {
-    return p.Persona.underwriter;
-  } else if (_persona === "businessAnalyst") {
-    return p.Persona.businessAnalyst;
-  } else if (_persona === "admin") {
-    return p.Persona.admin;
-  } else {
-    return p.Persona.admin;
-    //TODO: figure out a default return
-  }
+    if (_persona === "underwriter") {
+        return p.Persona.underwriter;
+    } else if (_persona === "businessAnalyst") {
+        return p.Persona.businessAnalyst;
+    } else if (_persona === "admin") {
+        return p.Persona.admin;
+    } else {
+        return p.Persona.admin;
+        //TODO: figure out a default return
+    }
 }
 
 function statusHelper(_status: string | null): p.Status {
-  if (_status === "new") {
-    return p.Status.new;
-  } else if (_status === "inProgress") {
-    return p.Status.inProgress;
-  } else if (_status === "complete") {
-    return p.Status.complete;
-  } else {
-    return p.Status.new;
-    //TODO: figure out a default return
-  }
+    if (_status === "new") {
+        return p.Status.new;
+    } else if (_status === "inProgress") {
+        return p.Status.inProgress;
+    } else if (_status === "complete") {
+        return p.Status.complete;
+    } else {
+        return p.Status.new;
+        //TODO: figure out a default return
+    }
 }
