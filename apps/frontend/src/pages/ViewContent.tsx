77d33b44579eaf62e5461cd513aedc24c45e44cd
LukeCiar <lukemciarletta@gmail.com>
2026-04-13 20:39:08 -0400

    Addded backend hook

--- apps/frontend/src/pages/ViewContent.tsx
+++ apps/frontend/src/pages/ViewContent.tsx
@@ -116,8 +116,6 @@ function ViewContent() {
         item.displayName.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-
-
     const advancedFilteredContent = searchedContent.filter((item) => {
         const matchesStatus =
             advancedFilters.status.length === 0 ||
