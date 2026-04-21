251a4e22c30308eeb9ad7786fb24722ddfa4e4aa
KrazyKoi <cpietraski@gmail.com>
2026-04-21 12:04:50 -0400

    added button, needs to be moved

---
+++ apps/frontend/src/components/layout/DarkmodeButton.tsx
@@ -0,0 +1,26 @@
+import {Sun, Moon} from "lucide-react";
+import {useState} from "react";
+
+function DarkmodeButton() {
+
+    const [isDark, setDark] = useState(false);
+
+    const handleClick = () => {
+        document.documentElement.classList.toggle("dark");
+        setDark(!isDark);
+    }
+
+    const IconComponent = isDark ? Moon : Sun;
+
+    return (
+        <div className="relative flex items-center">
+            <div className={`absolute flex items-center`}
+                 onClick={handleClick}
+            >
+                <IconComponent />
+            </div>
+        </div>
+    )
+}
+
+export default DarkmodeButton;
