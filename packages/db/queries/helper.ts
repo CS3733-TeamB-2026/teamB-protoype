e551523254415806e59d2da6a215b81a00d93ad9
Dylan <dmzickus@wpi.edu>
2026-04-20 21:33:14 -0400

    Add interface to add a service req to the db

--- packages/db/queries/helper.ts
+++ packages/db/queries/helper.ts
@@ -35,4 +35,17 @@ export class Helper {
         }
 
     }
+
+    public static requestHelper(_type: string | null): p.RequestType | null {
+        if (_type == "reviewClaim") {
+            return p.RequestType.reviewClaim
+        } else if (_type == "requestAdjuster") {
+            return p.RequestType.requestAdjuster
+        } else if (_type == "checkClaim") {
+            return p.RequestType.checkClaim
+        } else {
+            console.log(_type)
+            return null
+        }
+    }
 }
