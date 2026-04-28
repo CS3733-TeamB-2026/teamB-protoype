6958e230c6e276fa0aa3630168cc638edafc9d84
JSwanson06 <sjake9637@gmail.com>
2026-04-28 14:01:13 -0400

    Tutorial Page Work

---
+++ apps/frontend/src/components/ui/slide-deck.tsx
@@ -0,0 +1,35 @@
+import slide1 from "../../assets/Team Photos/Cameron Pietraski.png";
+import slide2 from "../../assets/Team Photos/Dylan Zickus.jpg";
+import slide3 from "../../assets/Team Photos/Hayden Schultz.jpg";
+import slide4 from "../../assets/Team Photos/Jake Swanson.jpg";
+import slide5 from "../../assets/Team Photos/Joey Hemmerle.jpg";
+import slide6 from "../../assets/Team Photos/Luke Ciarletta.png";
+import slide7 from "../../assets/Team Photos/Nicholas Houghton.jpeg";
+import slide8 from "../../assets/Team Photos/Oscar Stomberg.jpg";
+import slide9 from "../../assets/Team Photos/Philip Ostrowski.jpg";
+import slide10 from "../../assets/Team Photos/Ricardo Guzman Volpe.jpeg";
+import slide11 from "../../assets/Team Photos/Ricardo Guzman Volpe.jpeg";
+import slide12 from "../../assets/Team Photos/Ricardo Guzman Volpe.jpeg";
+import slide13 from "../../assets/Team Photos/Ricardo Guzman Volpe.jpeg";
+import slide14 from "../../assets/Team Photos/Ricardo Guzman Volpe.jpeg";
+import slide15 from "../../assets/Team Photos/Ricardo Guzman Volpe.jpeg";
+import slide16 from "../../assets/Team Photos/Ricardo Guzman Volpe.jpeg";
+
+export const slideDecks = {
+    slide1,
+    slide2,
+    slide3,
+    slide4,
+    slide5,
+    slide6,
+    slide7,
+    slide8,
+    slide9,
+    slide10,
+    slide11,
+    slide12,
+    slide13,
+    slide14,
+    slide15,
+    slide16,
+};
