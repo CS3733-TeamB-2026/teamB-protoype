d6c4d5ac59a815dcae5a6b2d75973add33275c8a
Dylan <dmzickus@wpi.edu>
2026-04-28 21:18:04 -0400

    remove hits column

--- packages/db/queries/content.ts
+++ packages/db/queries/content.ts
@@ -43,7 +43,6 @@ export class Content {
             "ownerId",
             "checkedOutById",
             "checkedOutAt",
-            "hits",
             1 - ("embedding" <=> ${embeddingStr}::vector) AS similarity
         FROM "Content"
         WHERE "embedding" IS NOT NULL
