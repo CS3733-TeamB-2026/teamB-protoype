5a6c6452d456a6da52b06776facd9e02f5c74921
nhoughton1 <MACARONIHOUGHTON@GMAIL.COM>
2026-05-03 15:54:32 -0400

    More question marks

--- apps/frontend/src/features/dashboard/components/cards/MyContentCard.tsx
+++ apps/frontend/src/features/dashboard/components/cards/MyContentCard.tsx
@@ -1,6 +1,7 @@
 import {CardContent, CardHeader} from "@/components/ui/card.tsx";
 import MyFiles from "@/features/content/listing/MyFiles.tsx";
 import DashboardCard from "@/features/dashboard/components/cards/DashboardCard.tsx";
+import InfoButton from "@/components/layout/InformationAlert.tsx";
 
 function MyContentCard() {
     return (
@@ -10,6 +11,9 @@ function MyContentCard() {
         >
             <CardHeader className="text-left text-2xl! font-semibold">My Files: </CardHeader>
             <CardContent>
+                <div className="absolute right-1 top-1 w-8 h-8 cursor-pointer">
+                    <InfoButton content={"Shows content that you own"}/>
+                </div>
                 <MyFiles/>
             </CardContent>
         </DashboardCard>
--- apps/frontend/src/features/dashboard/components/cards/PreviewedFilesCard.tsx
+++ apps/frontend/src/features/dashboard/components/cards/PreviewedFilesCard.tsx
@@ -3,12 +3,16 @@ import {Link} from "react-router-dom";
 import {Button} from "@/components/ui/button.tsx";
 import {FolderOpen} from "lucide-react";
 import PreviewedFiles from "@/features/content/listing/PreviewedFiles.tsx";
+import InfoButton from "@/components/layout/InformationAlert.tsx";
 
 function PreviewedFilesCard() {
     return (
         <Card className="border-t-secondary border-t-4 shadow-lg hover:scale-101 transition-transform md:col-span-2 px-4 py-8">
             <CardHeader className="text-left text-2xl! font-semibold">Recently Viewed: </CardHeader>
             <CardContent>
+                <div className="absolute right-1 top-1 w-8 h-8 cursor-pointer">
+                    <InfoButton content={"Shows recently viewed files"}/>
+                </div>
                 <PreviewedFiles />
                 <Link to="/files" className="w-full">
                     <Button className="mt-5 w-full justify-start gap-3 px-4 py-5 rounded-xl bg-primary/5 border border-primary/20 text-primary hover:bg-primary hover:text-primary-foreground transition-all active:brightness-90 shadow-none" variant="outline">
